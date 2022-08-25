8972c5b5986c1201babae39b69f325cbdca5c534
T.M Siddiquee <siddiquee09@gmail.com>
2022-08-26 00:02:11 +0600

    selected part

--- js/player.js
+++ js/player.js
@@ -1,7 +1,48 @@
 
 const selected = []
+
+
+function display(names) {
+
+    const selectedPlayers = document.getElementById('selected-players')
+    selectedPlayers.innerText = ""
+    
+    for (let i = 0; i < names.length; i++){
+
+        const li = document.createElement('li')
+        
+        li.innerHTML = names[i]
+
+        selectedPlayers.appendChild(li)
+    }
+}
+
 function addToSelect(click) {
     const playerName = click.parentNode.children[0].innerText
+    
     selected.push(playerName)
-    console.log(selected)
+    const totalPlayer = document.getElementById('total-player')
+
+    if (selected.length > 5) {
+        
+        alert("Cannot Select More Than 5!")
+    }
+
+    else {
+       
+        totalPlayer.innerText = selected.length
+        display(selected)
+    }
+
+    return (selected.length)
+    
 }
+
+
+document.getElementById('calculate').addEventListener('click', function(){
+
+    const playerPrice = document.getElementById('player-price')
+
+    const playerPriceInput = parseInt(playerPrice.value)
+    console.log(playerPriceInput)
+})
